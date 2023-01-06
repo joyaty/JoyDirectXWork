e735d9963ffb469468e2fc02ec27565ea4470b07
joyaty <joyaty@gmail.com>
2023-01-06 13:48:39 +0800

    * 添加获取逆转置矩阵的工具函数

--- DirectXWork/Lib_DXBaseApp/Source/DirectXBaseWork/MathUtil.h
+++ DirectXWork/Lib_DXBaseApp/Source/DirectXBaseWork/MathUtil.h
@@ -28,6 +28,22 @@ public:
 		return Identity4X4;
 	}
 
+	/// <summary>
+	/// 获取矩阵M的逆转置矩阵
+	/// </summary>
+	/// <param name="M"></param>
+	/// <returns></returns>
+	static DirectX::XMMATRIX InverseTranspose(DirectX::CXMMATRIX M)
+	{
+		DirectX::XMMATRIX A = M;
+		// 将平移项置零，逆转置矩阵用于法线变换，法线是一个向量，平移操作无意义，置零可以避免逆转置矩阵与其他矩阵链接时，发生位移渗入导致错误
+		A.r[3] = DirectX::XMVectorSet(0.f, 0.f, 0.f, 1.f);
+		// 计算矩阵A的行列式
+		DirectX::XMVECTOR det = DirectX::XMMatrixDeterminant(A);
+		// 先计算逆矩阵，再计算转置矩阵
+		return DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(&det, A));
+	}
+
 public:
 	template<typename T> static T Clamp(const T& value, const T& min, const T& max)
 	{
